47d7be5f370194b50f8a4053c5e9dcf29846f80d
andrewtong20 <andrewtongat2009@gmail.com>
2019-11-19 14:24:51 -0600

    third commit

--- HelloWorldAT/src/helloworldat/HelloWorldAT.java
+++ HelloWorldAT/src/helloworldat/HelloWorldAT.java
@@ -16,6 +16,11 @@ public class HelloWorldAT {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        for (int i=1; i<11; i++)
+        {
+            System.out.println(i);
+        }
     }
     
 }
