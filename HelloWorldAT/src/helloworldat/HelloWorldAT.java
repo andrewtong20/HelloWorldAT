fd187a583831a6b132d11545e0fea24d2fb54d94
andrewtong20 <andrewtongat2009@gmail.com>
2019-11-19 14:26:59 -0600

    deleted everything

--- HelloWorldAT/src/helloworldat/HelloWorldAT.java
+++ HelloWorldAT/src/helloworldat/HelloWorldAT.java
@@ -15,12 +15,9 @@ public class HelloWorldAT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         
-        for (int i=1; i<11; i++)
-        {
-            System.out.println(i);
-        }
+        //LMAO everything is gone
+
     }
     
 }
